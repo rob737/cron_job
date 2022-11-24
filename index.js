26980c064a795d4d3680317c75b131ec6113d121
rob737 <robinsrivastava737@gmail.com>
2022-11-24 08:59:40 +0530

    entry point

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * This is the starting point of project.
+ * This is where project actually starts it's execution from.
+ * 1. API to trigger start of cron job
+ * 2. API to stop cron job altogether
+ */
+
+/**
+ * Implementation specifics :
+ * 1. we need server to host our code
+ * 2. Understand how connections are being made
+ * 3. Understand how request is being parsed and understood
+ */
